d0d187f694953fc06f35b11b16ef2944529707ec
Santiago Bang <sbang@mail.austral.edu.ar>
2025-11-28 22:06:13 -0300

    minor changes

--- db/init/01-init-snippet.sql
+++ db/init/01-init-snippet.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS snippet (
     id_snippet SERIAL PRIMARY KEY,
     id_bucket INT,
     formatter_applied BOOLEAN,
-    linter_applied BOOLEAN,
+    linter_applied BOOLEAN
     );
 
