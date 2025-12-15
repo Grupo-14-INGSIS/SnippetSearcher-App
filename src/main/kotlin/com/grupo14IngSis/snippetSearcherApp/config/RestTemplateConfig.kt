2422203ff365e5a8f6866febe0c8102202d2e829
SergioMorero <smorero@mail.austral.edu.ar>
2025-12-15 13:33:39 -0300

    Added Apache's HTTP client

--- src/main/kotlin/com/grupo14IngSis/snippetSearcherApp/config/RestTemplateConfig.kt
+++ src/main/kotlin/com/grupo14IngSis/snippetSearcherApp/config/RestTemplateConfig.kt
@@ -2,10 +2,15 @@ package com.grupo14IngSis.snippetSearcherApp.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
 
 @Configuration
 class RestTemplateConfig {
     @Bean
-    fun restTemplate(): RestTemplate = RestTemplate()
+    fun restTemplate(): RestTemplate {
+        val restTemplate = RestTemplate()
+        restTemplate.requestFactory = HttpComponentsClientHttpRequestFactory()
+        return restTemplate
+    }
 }
